c4a97e7a1bed2c572564c0b3e3c4db4728f6e784
Hardik Gayner <hardikgayner@gmail.com>
2025-08-12 18:58:02 +0530

    Added typedefs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// types
+import { typeDefs } from './schema';
+
 // server setup
 const server = new ApolloServer({
     // typeDefs shows type definitions for the data types and relation they have with other data types
+    typeDefs
     // resolvers how we respond to queries and different data on the graph
 })
 
@@ -11,4 +15,4 @@ const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
 
-console.log(`🚀 Server ready at: ${url}`);
+console.log(`🚀 Server ready at: ${url}`); 
