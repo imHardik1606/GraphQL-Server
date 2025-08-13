240738de4cef87fda8ccc42f3d81d83156f85e00
Hardik Gayner <hardikgayner@gmail.com>
2025-08-13 19:54:29 +0530

    Wrote Delete & Add Mutation for Games

--- index.js
+++ index.js
@@ -52,6 +52,25 @@ const resolvers = {
       game(parent){
         return db.games.find((g) => g.id === parent.game_id)
       }
+    },
+
+    Mutation: {
+      deleteGame(_, args) {
+        db.games = db.games.filter((game)=> game.id !== args.id);
+
+        return db.games;
+      },
+
+      addGame(_, args){
+        let newGame = {
+          ...args.game,
+          id: Math.floor(Math.random() * 10000).toString()
+        }
+
+        db.games.push(newGame)
+
+        return newGame;
+      }
     }
 };
 
